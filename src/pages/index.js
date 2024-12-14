f4cb325c7fe5dbd0817c04b7bbdc5cf569a5e22a
yousaf <yousafsomroo35@gmail.com>
2024-12-14 00:56:51 -0800

    first three sections

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+// import HeroSection from '@/components/heroSection';
+
+
+import { useState } from 'react';
+import { Search } from 'lucide-react';
+import Navbar from '../components/Navbar';
+import HeroSection from '@/components/HeroSection';
+import { clashGrotesk } from '@/lib/clashGroteskfont';
+import { LogoMarquee } from '@/components/HeroLogoSection';
+import AnimatedCarousel from '@/components/Homecarousels';
+import WhatWeDo from '@/components/WhatWeDo';
+import { satoshi } from '@/lib/fonts';
+
+// import Footer from '@/components/Footer';
+
+
+export default function Home() {
+
+
+  return (
+    <main className={`${clashGrotesk.className} ${satoshi.className}`}>
+      <Navbar />
+      <HeroSection />
+      <LogoMarquee/>
+      <AnimatedCarousel/>
+      <WhatWeDo/>
+    </main>
+  )
+}
+
+
